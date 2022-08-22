975f189db3821154b9b223719179240f2ed8005b
Fabrice BG <fabricebg@users.noreply.github.com>
2022-08-22 15:12:11 -0400

    Create class-coins.php

---
+++ core/publications/class-coins.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * This file contains logic to export in Coins format.
+ *
+ * @license http://www.gnu.org/licenses/gpl-2.0.html GPLv2 or later
+ * @since 2.0.0
+ */
+
+/**
+ * teachPress Coins class
+ *
+ * @package teachpress\core\bibtex
+ * @since 9.0.0
+ */
+class TP_Coins {
+
+    
+
+}
